72d2158a72024739db6874b8aa71d6f02a4ca50f
Pranav Salunke <dguitarbite@gmail.com>
2016-09-12 16:04:46 +0200

    Updates to terraform configuration.
    Now terraform libvirt provider should do the following:
    
    - Create a custom NAT network for libvirt.
    - Create a cloudinit.iso with required parameters like ssh key
      injection.
    - Attach network interfaces and cloudinit.iso to the guests.
    
    With these changes, fetching the IP addresses should become easier and
    what follows is of course installing saltstack.

--- libvirt.tf
+++ libvirt.tf
@@ -9,6 +9,21 @@ resource "libvirt_volume" "opensuse_leap" {
 
 }
 
+resource "libvirt_cloudinit" "commoninit" {
+
+    name = "commoninit.iso"
+    local_hostname = "tfsaltdemo"
+    ssh_authorized_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCwxtBlrwiGRN9LUsbkVYcDah2Q+BduBWQH5/jryqORyeBIy4iYWqnzIpc+bz+fNr0t7jvv/f4B0c4TT65bgyfoyjy6kDhMQ9K4JnR2LeKH3wtE2ff9+4FyweONyrxB/GQyY51zGBX+qEdyidrt3I4dHh0libbekokmDb+xRTWdLABi9tXP7nMfBm08ML2S/+UcCyZqtSoDlVmXU6lIpf9CiWAQEPymEE5nB0hplMvzCz0jCzgBBu1wgEfPeMMjgHGRtUr6ESzJIm1zJTPpn3evHas30iIxqUW3OmqGaaWK1QIBfnP+fw+Q8YLPu24Mk2Cs2lDZfa3xUXSVCHzJ34Yh pranav@pranavws"
+}
+
+resource "libvirt_network" "tfsaltdemo" {
+
+    name = "tfsaltdemo"
+    mode = "nat"
+    domain = "tfsaltdemo.local"
+    addresses = ["10.5.4.0/24"]
+}
+
 resource "libvirt_volume" "mastervolume" {
 
     name = "dsaltmastervol"
@@ -28,7 +43,17 @@ resource "libvirt_domain" "saltmaster" {
     disk {
         volume_id = "${libvirt_volume.mastervolume.id}"
     }
+    network_interface {
+        network_id = "${libvirt_network.tfsaltdemo.id}"
+        hostname = "saltmaster"
+        addresses = ["10.5.4.3"]
+        wait_for_lease = 1
+    }
+    cloudinit = "${libvirt_cloudinit.commoninit.id}"
     count = 1
+    provisioner "local-exec" {
+        command = "echo 'Hello'"
+    }
 }
 
 resource "libvirt_domain" "saltminion" {
@@ -37,5 +62,25 @@ resource "libvirt_domain" "saltminion" {
     disk {
         volume_id = "${element(libvirt_volume.minionvolume.*.id, count.index)}"
     }
+    network_interface {
+        network_id = "${libvirt_network.tfsaltdemo.id}"
+        hostname = "saltminion${count.index}"
+        addresses = ["10.5.4.${count.index+4}"]
+        wait_for_lease = 1
+    }
+    cloudinit = "${libvirt_cloudinit.commoninit.id}"
     count = 2
+    provisioner "local-exec" {
+        command = "echo 'Hello'"
+    }
+}
+
+output "master" {
+
+    value = "${libvirt_domain.saltmaster.ip}"
+}
+
+output "minions" {
+
+    value = "${libvirt_domain.saltminion.ip}"
 }
